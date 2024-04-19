256f3c6340e752706a49a195e2343f87d0ad71ac
deepa126 <165250722+deepa126@users.noreply.github.com>
2024-04-19 03:06:23 -0700

    Add files via upload
    ok

---
+++ python.py
@@ -0,0 +1,24 @@
+import calendar
+def print_calender(year, month):
+    cal = calendar.month(year, month)
+    return cal
+
+year=2023
+month=3
+output = print_calender(year,month)
+print(output)
+
+year=2023
+month=4
+output = print_calender(year,month)
+print(output)\
+
+year=2023
+month=5
+output = print_calender(year,month)
+print(output)
+
+year=2023
+month=6
+output = print_calender(year,month)
+print(output)
